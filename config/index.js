7d6c5342bd1f9daa351b6637655e66e5d7a06531
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-01-05 08:06:57 +0100

    Unnecessary variable

--- config/index.js
+++ config/index.js
@@ -1,13 +1,10 @@
 'use strict'
 
-const envs = process.env
-
 module.exports = {
-  QUEUE_DIRECTORY_PATH: envs.QUEUE_DIRECTORY_PATH || 'test/data/queue',
-  STATUS_DIRECTORY_PATH: envs.STATUS_DIRECTORY_PATH || 'test/data/status',
-  ERRORS_DIRECTORY_PATH: envs.ERRORS_DIRECTORY_PATH || 'test/data/errors',
-  DONE_DIRECTORY_PATH: envs.DONE_DIRECTORY_PATH || 'test/data/done',
-  p360: {
+  QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/data/queue',
+  STATUS_DIRECTORY_PATH: process.env.STATUS_DIRECTORY_PATH || 'test/data/status',
+  ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/data/errors',
+  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
     user: process.env.TFK_LAURENTIUS_P360WS_USER || 'domain/user', // username
     password: process.env.TFK_LAURENTIUS_P360WS_PASSWORD || 'password', // passord
     baseUrl: process.env.TFK_LAURENTIUS_P360WS_BASEURL || 'http://tfk-fh-siweb01t.login.top.no:8088/SI.WS.Core/SIF/',
